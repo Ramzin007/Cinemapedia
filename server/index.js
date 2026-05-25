3aab0ea155df9dcb2bd93fad5bd3de036a57d3a9
Ramzin007 <muhammedramzinp9@gmail.com>
2026-05-25 13:27:16 +0530

    Add dotenv configuration to load environment variables

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 
 import movieRoutes from "./routes/movieRoutes.js";
+import dotenv from 'dotenv';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 app.use("/api/movies", movieRoutes);
 
