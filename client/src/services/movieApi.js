4b952d24a2ebe9f45516a5a269f5bf1ca6ecf9f4
Ramzin007 <muhammedramzinp9@gmail.com>
2026-06-04 17:21:48 +0530

    Prepare project for deployment

--- client/src/services/movieApi.js
+++ client/src/services/movieApi.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/movies";
-const LIBRARY_API_URL = "http://localhost:5000/api/library";
-const AUTH_API_URL = "http://localhost:5000/api/auth";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const API_BASE_URL = `${BASE_URL}/api/movies`;
+const LIBRARY_API_URL = `${BASE_URL}/api/library`;
+const AUTH_API_URL = `${BASE_URL}/api/auth`;
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
